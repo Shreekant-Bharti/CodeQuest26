32abb613ed92ca312f202e47ab14577973b34721
Rahul Raj <raahulll3030@gmail.com>
2026-05-10 15:19:52 +0530

    Update hashmap_frequency.cpp

--- CPP/hashmap_frequency.cpp
+++ CPP/hashmap_frequency.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 unordered_map<int, int> countFrequencies(vector<int>& arr) {
     vector<int> freq;
-    for(int i=0; i<=arr.size(); i++) {
+    for(int i=0; i<arr.size(); i++) {
         int num = arr[i];
         if(freq.count(num)) {
-            freq[num] = 1;
+            freq[num] += 1;
         } else {
             freq[num] = 0;
         }
