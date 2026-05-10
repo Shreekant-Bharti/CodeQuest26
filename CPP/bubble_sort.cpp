cdaaa8a75ce9a353c2694fd8b6ea0ca8e043dc8b
Pratham Kumar Rana <userpratham4016@gmail.com>
2026-05-10 15:18:08 +0530

    Correct bubble sort implementation
    Fix bubble sort logic and return the sorted array.

--- CPP/bubble_sort.cpp
+++ CPP/bubble_sort.cpp
@@ -1,15 +1,16 @@
 #include <vector>
+#include<iostream>
 using namespace std;
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
     for(int i=0; i<n; i++) {
-        for(int j=0; j<n-i; j++) {
-            if(arr[j] < arr[j+1]) {
-                int temp = arr[j];
-                arr[j+1] = arr[j+1];
-                arr[j+1] = temp;
+        for(int j=i+1; j<n; j++) { // j start with i+1 not with 0
+            if(arr[i] > arr[j]) { //arr[i] not arr[j] and arr[i]>arr[j] not >arr[j+1]
+                int temp = arr[i];  //arr[i] not arr[j]
+                arr[i] = arr[j];
+                arr[j] = temp; //index out of range if arr[j+1]
             }
         }
-        return;
+        return arr; // will return array itself at the end
     }
 }
