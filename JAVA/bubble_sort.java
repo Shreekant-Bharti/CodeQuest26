637a4ed01f45d10c5c5b40f99c71d7824ba66a97
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2026-05-10 15:07:51 +0530

    Correct bubble sort implementation
    Fix bubble sort logic and ensure proper swapping.

--- JAVA/bubble_sort.java
+++ JAVA/bubble_sort.java
@@ -1,15 +1,15 @@
 class Solution {
     public void bubbleSort(int[] arr) {
         int n = arr.length;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n-i; j++) {
+        for(int i=0; i<n - 1; i++) {
+            for(int j=0; j<n-i-1; j++) {
                 if(arr[j] < arr[j+1]) {
                     int temp = arr[j];
-                    arr[j+1] = arr[j+1];
+                    arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
             }
-            return;
         }
     }
 }
+// 4
