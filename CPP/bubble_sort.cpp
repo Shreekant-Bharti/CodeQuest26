7cb3e670d6aa1014ba940a6c978fffa60f5d0145
Pratham Kumar Rana <userpratham4016@gmail.com>
2026-05-10 15:19:09 +0530

    Correct outer loop condition in bubble sort
    Fix bubble sort loop to avoid unnecessary iterations.

--- CPP/bubble_sort.cpp
+++ CPP/bubble_sort.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<n-1; i++) {
         for(int j=i+1; j<n; j++) { // j start with i+1 not with 0
             if(arr[i] > arr[j]) { //arr[i] not arr[j] and arr[i]>arr[j] not >arr[j+1]
                 int temp = arr[i];  //arr[i] not arr[j]
