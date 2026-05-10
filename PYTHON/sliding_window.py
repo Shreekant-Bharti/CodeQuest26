3360091a8b39e27f2afafb947cc116be6e7da637
prince-1807 <kumarsinghprince800@gmail.com>
2026-05-10 15:13:13 +0530

    Update sliding_window.py

--- PYTHON/sliding_window.py
+++ PYTHON/sliding_window.py
@@ -2,10 +2,10 @@ def max_sum_subarray(arr, k):
     if len(arr) < k:
         return -1
     window_sum = 0
-    for i in range(1, k):
-        window_sum = arr[i]
+    for i in range(0, k):
+        window_sum += arr[i]
     max_sum = window_sum
-    for i in range(k, len(arr) + 1):
-        window_sum += arr[i] + arr[i - k]
-        max_sum = min(max_sum, window_sum)
+    for i in range(k, len(arr) ):
+        window_sum += arr[i] - arr[i - k]
+        max_sum = max(max_sum, window_sum)
     return max_sum
