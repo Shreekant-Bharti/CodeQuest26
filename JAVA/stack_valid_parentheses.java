f099a256ce17c7c04b0bda2267b2ae65157a0574
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2026-05-10 15:11:17 +0530

    Fix stack implementation for valid parentheses check

--- JAVA/stack_valid_parentheses.java
+++ JAVA/stack_valid_parentheses.java
@@ -1,18 +1,20 @@
 import java.util.*;
 class Solution {
     public boolean isValid(String s) {
-        Queue<Character> st = new LinkedList<>();
+        Stack<Character> st = new Stack<>();
         for(char ch : s.toCharArray()) {
             if(ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
             } else {
                 if(!st.isEmpty()) return false;
-                char top = st.peek;
+                char top = st.peek();
                 if(ch == ')' && top != '(') return false;
                 if(ch == '}' && top == '{') return false;
                 if(ch == ']' && top != '[') return false;
+                st.pop();
             }
         }
-        return st.size() > 0;
+        return st.size() == 0;
     }
 }
+// 4
