04f6c42b13ae46a8e706695429ba586c4e654a32
Pratham Kumar Rana <userpratham4016@gmail.com>
2026-05-10 15:30:31 +0530

    Fix sliding window implementation for max sum

--- CPP/sliding_window.cpp
+++ CPP/sliding_window.cpp
@@ -2,15 +2,15 @@
 #include <algorithm>
 using namespace std;
 int maxSumSubarray(vector<int>& arr, int k) {
-    if(arr.size() < k) return -1;
+    if(arr.size() > k) return -1;  // arr.size()>k not <k
     int windowSum = 0;
-    for(int i=1; i<k; i++) {
-        windowSum = arr[i];
+    for(int i=0; i<k; i++) {  // i will start with 0
+        windowSum += arr[i]; //+=
     }
     int maxSum = windowSum;
-    for(int i=k; i<=arr.size(); i++) {
+    for(int i=k; i<arr.size(); i++) { // less than, index out of bound
         windowSum += arr[i] + arr[i-k];
-        maxSum = min(maxSum, windowSum);
+        maxSum = max(maxSum, windowSum);  //max function
     }
     return maxSum;
 }
