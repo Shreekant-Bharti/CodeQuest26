b1db5b7abedb0877b5712c0548daa9b3f16654b8
Rahul Raj <raahulll3030@gmail.com>
2026-05-10 16:06:27 +0530

    Update stack_valid_parentheses.cpp

--- CPP/stack_valid_parentheses.cpp
+++ CPP/stack_valid_parentheses.cpp
@@ -10,9 +10,9 @@ bool isValid(string s) {
             if(!st.empty()) return true;
             char top = st.top();
             st.pop();
-            if(ch == ')' && top != '(') return false;
-            if(ch == '}' && top != '{') return false;
-            if(ch == ']' && top != '[') return false;
+            if(ch == ')' && top == '(') return false;
+            if(ch == '}' && top == '{') return false;
+            if(ch == ']' && top == '[') return false;
         }
     }
     return st.size() > 0;
