08847e2cd22c638ae2d0d4c4c65bfa1f46c636a8
roushan2028 <roushankumarsahu20@gmail.com>
2026-05-10 15:26:48 +0530

    Update stack_valid_parentheses.cpp

--- CPP/stack_valid_parentheses.cpp
+++ CPP/stack_valid_parentheses.cpp
@@ -1,19 +1,18 @@
-#include <string>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 bool isValid(string s) {
-    queue<char> st;
+    stack<char> st;
     for(char ch : s) {
         if(ch == '(' || ch == '{' || ch == '[') {
             st.push(ch);
         } else {
-            if(!st.empty()) return false;
+            if(st.empty()) return false;
             char top = st.top;
             st.pop();
             if(ch == ')' && top != '(') return false;
-            if(ch == '}' && top == '{') return false;
+            if(ch == '}' && top != '{') return false;
             if(ch == ']' && top != '[') return false;
         }
     }
-    return st.size() > 0;
+    return st.size() == 0;
 }
