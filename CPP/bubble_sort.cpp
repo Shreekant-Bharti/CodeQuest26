84fe9ded96249a804c66f18dd55f15071fc6b26b
sourav191007 <kumarsourav191007@gmail.com>
2026-05-10 15:08:34 +0530

    Update bubble_sort.cpp

--- CPP/bubble_sort.cpp
+++ CPP/bubble_sort.cpp
@@ -1,10 +1,11 @@
+#include<bits/stdc++.h>
 #include <vector>
 using namespace std;
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
     for(int i=0; i<n; i++) {
         for(int j=0; j<n-i; j++) {
-            if(arr[j] < arr[j+1]) {
+            if(arr[j] > arr[j+1]) {
                 int temp = arr[j];
                 arr[j+1] = arr[j+1];
                 arr[j+1] = temp;
