dd8a1b6074f9b57fc3a2c27b395538f442c1378c
aashish890bit <aashish890.bit@gmail.com>
2026-05-10 15:30:33 +0530

    Update sliding_window.cpp

--- CPP/sliding_window.cpp
+++ CPP/sliding_window.cpp
@@ -5,7 +5,7 @@ int maxSumSubarray(vector<int>& arr, int k) {
     if(arr.size() < k) return -1;
     int windowSum = 0;
     for(int i=1; i<k; i++) {
-        windowSum = arr[i];
+        windowSum += arr[i];
     }
     int maxSum = windowSum;
     for(int i=k; i<arr.size(); i++) {
