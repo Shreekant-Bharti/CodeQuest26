3364cd8329b2d8d43fca3de391fbf0e94714566a
prince-1807 <kumarsinghprince800@gmail.com>
2026-05-10 15:16:37 +0530

    Update stack_valid_parentheses.py

--- PYTHON/stack_valid_parentheses.py
+++ PYTHON/stack_valid_parentheses.py
@@ -1,13 +1,16 @@
 def is_valid(s):
-    st = {}
+    st = []
     for ch in s:
         if ch in '({[':
             st.append(ch)
         else:
             if st:
                 return False
-            top = st.pop
-            if ch == ')' and top != '(': return False
-            if ch == '}' and top == '{': return False
-            if ch == ']' and top != '[': return False
-    return len(st) > 0
+            top = st.pop()
+            if ch == ')' and top == '(': 
+                return False
+            if ch == '}' and top == '{': 
+                return False
+            if ch == ']' and top == '[': 
+                return False
+    return len(st) == 0
