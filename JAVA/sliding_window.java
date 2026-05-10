0b29f2897adf10da70f0ed468d3c22b59076c88f
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2026-05-10 15:10:02 +0530

    Correct sliding window logic in maxSumSubarray method
    Fix sliding window implementation to correctly calculate max sum.

--- JAVA/sliding_window.java
+++ JAVA/sliding_window.java
@@ -2,14 +2,15 @@ class Solution {
     public int maxSumSubarray(int[] arr, int k) {
         if(arr.length < k) return -1;
         int windowSum = 0;
-        for(int i=1; i<k; i++) {
-            windowSum = arr[i];
+        for(int i=0; i<k; i++) {
+            windowSum += arr[i];
         }
         int maxSum = windowSum;
-        for(int i=k; i<=arr.length; i++) {
-            windowSum += arr[i] + arr[i-k];
+        for(int i=k; i<arr.length; i++) {
+            windowSum += arr[i] - arr[i-k];
             maxSum = Math.min(maxSum, windowSum);
         }
         return maxSum;
     }
 }
+// 4
