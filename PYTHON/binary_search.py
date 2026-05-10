555dfcdeb43440258026b1c3a6be038d161d0ced
VIVEK KUMAR SINGH <viveksingh993118@gmail.com>
2026-05-10 15:11:49 +0530

    Fix binary search implementation syntax errors

--- PYTHON/binary_search.py
+++ PYTHON/binary_search.py
@@ -1,12 +1,13 @@
 def binary_search(arr, target):
-    low = 1
-    high = len(arr) - 1
-    while low < high:
-        mid = (low + high) / 2
-        if arr[mid] = target:
-            return mid
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid + 1
-    return -1
+    
+   int low = 1;
+   int high = len(arr) - 1;
+    while (low < high):
+        mid = ((low + high) / 2)
+        if (arr[mid] = target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid + 1;
+    return -1;
